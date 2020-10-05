02db1daeac08fa9c0f35999e15e5107a82509823
Sergio <sergiotribaldosgarcia@gmail.com>
2020-10-05 10:32:31 +0200

    Feat (Collaborators modal): Collaborators modal implemented

--- src/app/pages/shopping-list/components/modal/modal.component.ts
+++ src/app/pages/shopping-list/components/modal/modal.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+import {Observable} from 'rxjs';
+import {ShoppingList} from '../../shopping-list.component';
+import {take, tap} from 'rxjs/operators';
+import {FirebaseService} from '../../../../services/firebase.service';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss'],
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
+    @Input() shoppingList: Observable<any>;
+    @Input() listId: string;
 
-  constructor() { }
+    constructor(private modalController: ModalController, private firebaseService: FirebaseService) {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
 
+    closeModal() {
+        this.modalController.dismiss();
+    }
+
+    addMember(email: string|number) {
+        const newMemberData = {
+            [email]: {
+                accountCreator: false,
+                confirmed: false,
+                exist: true,
+            }
+        };
+
+        this.shoppingList.pipe(
+            take(1),
+            tap((shoppingList) => {
+                const listData = {
+                    createdBy: shoppingList.createdBy,
+                    listName: shoppingList.listName,
+                    listId: this.listId
+                };
+
+                this.firebaseService.addCollaborator(newMemberData, listData);
+            })
+        ).subscribe();
+
+    }
+
+    deleteUser(memberId: string) {
+        this.firebaseService.deleteCollaborator(this.listId, memberId);
+    }
 }
