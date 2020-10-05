1f9d2838458ba3bcc196debb104d74e1630d40cf
Sergio <sergiotribaldosgarcia@gmail.com>
2020-10-05 10:29:57 +0200

    Feat(Invitations modal): Invitations modal component implemented

---
+++ src/app/pages/home/components/invitations-modal/invitations-modal.component.ts
@@ -0,0 +1,34 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+import { Observable} from 'rxjs';
+import {Invitation} from '../../home.page';
+import {FirebaseService} from '../../../../services/firebase.service';
+
+
+@Component({
+    selector: 'app-invitations-modal',
+    templateUrl: './invitations-modal.component.html',
+    styleUrls: ['./invitations-modal.component.scss'],
+})
+export class InvitationsModalComponent implements OnInit {
+
+    @Input() invitations: Observable<Invitation[]>;
+    @Input() photoURL: string;
+    @Input() displayName: string;
+
+    constructor(private modalController: ModalController, private firebaseService: FirebaseService) {
+    }
+
+    ngOnInit() {
+
+    }
+
+    closeModal() {
+        this.modalController.dismiss();
+    }
+
+    acceptInvitation(invitation: Invitation) {
+        this.firebaseService.acceptInvitation(invitation, this.photoURL, this.displayName);
+
+    }
+}
